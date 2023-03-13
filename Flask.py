907fe37fb3dbd537a4d7c128bf5bd3b55faa372c
Turkan1961 <119409298+Turkan1961@users.noreply.github.com>
2023-03-13 23:08:03 +0500

    new

--- Flask.py
+++ Flask.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from jinja2 import Template
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return 'index'
+
+@app.route('/about')
+def about():
+    return '<h1>Про Flask<h1>'
 if __name__ == '__main__':
     app.run(debug=True)
