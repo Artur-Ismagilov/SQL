6fcb4958b28f51061b64d2308166670e07343dff
Turkan1961 <119409298+Turkan1961@users.noreply.github.com>
2023-03-14 22:34:34 +0500

    Flask step2

--- Flask.py
+++ Flask.py
@@ -1,13 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 from jinja2 import Template
+
 app = Flask(__name__)
+
+menu = ['Установка', "Первое приложение", "Обратная связь"]
 
 @app.route("/")
 def index():
-    return 'index'
+    return render_template('index.html', menu=menu)
 
 @app.route('/about')
 def about():
-    return '<h1>Про Flask<h1>'
+    return render_template('about.html', title='О сайте', menu=menu)
 if __name__ == '__main__':
     app.run(debug=True)
