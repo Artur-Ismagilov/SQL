44903496a59ec197d7b5d2655df1ac3712846974
Turkan1961 <119409298+Turkan1961@users.noreply.github.com>
2023-03-16 23:14:31 +0500

    перенаправление

--- Flask.py
+++ Flask.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'fdgdfgdfggf786hfg6hfg6h7f'
 
 menu = [{"name": "Установка", "url": "install-flask"},
         {"name": "Первое приложение", "url": "first-app"},
@@ -20,11 +21,37 @@ def about():
 @app.route('/contact', methods=["POST", "GET"])
 def contact():
     if request.method == 'POST':
+        if len(request.form['username']) > 2:
+            flash('Сообщение отправлено', category='success')
+        else:
+            flash('Ошибка отправки', category='error')
+
         print(request.form)
     return render_template('contact.html', title='Обратная связь', menu=menu)
+
+
 @app.route("/profile/<username>")
 def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+
     return f"Пользователь: {username}"
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template('page404.html', title='Страница не найдена', menu=menu), 404
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == "selfedu" and request.form['psw'] == "123":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title="Авторизация", menu=menu)
+
 
 
 if __name__ == '__main__':
