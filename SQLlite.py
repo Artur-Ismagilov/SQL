430918544e94ea908c8451db4cf0270bcbf4e1e4
Turkan1961 <119409298+Turkan1961@users.noreply.github.com>
2023-03-17 21:40:28 +0500

    cars

--- SQLlite.py
+++ SQLlite.py
@@ -1,12 +1,16 @@
 import sqlite3 as sq
 
-with sq.connect("saper.db") as con:
+with sq.connect("cars.db") as con:
     cur = con.cursor()
-    cur.execute("""CREATE TABLE users (
-        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT not null,
-        sex integer default 1,
-        old integer,
-        score integer
-        ) 
-    """)
+
+    cur.execute("""CREATE TABLE IF NOT EXISTS cars (
+        car_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        model TEXT,
+        price INTEGER
+    )""")
+
+    cur.execute("INSERT INTO cars VALUES(1,'Audi',52642)")
+    cur.execute("INSERT INTO cars VALUES(2,'Mercedes',57127)")
+    cur.execute("INSERT INTO cars VALUES(3,'Skoda',9000)")
+    cur.execute("INSERT INTO cars VALUES(4,'Volvo',29000)")
+    cur.execute("INSERT INTO cars VALUES(5,'Bentley',350000)")
