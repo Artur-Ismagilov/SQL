59afd065e5704a3bd010d82f3b24108d2607dff7
Turkan1961 <119409298+Turkan1961@users.noreply.github.com>
2023-03-19 19:14:00 +0500

    Сайт 3

--- SQL_Flask.py
+++ SQL_Flask.py
@@ -1,6 +1,7 @@
 import sqlite3
 import os
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, request, g, flash
+from FDataBase import FDataBase
 
 # конфигурация
 DATABASE = '/tmp/flsite.db'
@@ -34,13 +35,42 @@ def get_db():
 @app.route("/")
 def index():
     db = get_db()
-    return render_template('index.html', menu = [])
+    dbase = FDataBase(db)
+    return render_template('index.html', menu = dbase.getMenu(), posts=dbase.getPostsAnonce())
+
+
+@app.route("/add_post", methods=["POST", "GET"])
+def addPost():
+    db = get_db()
+    dbase = FDataBase(db)
+
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'])
+            if not res:
+                flash('Ошибка добавления статьи', category='error')
+            else:
+                flash('Статья добавлена успешно', category='success')
+        else:
+            flash('Ошибка добавления статьи', category='error')
+
+    return render_template('add_post.html', menu=dbase.getMenu(), title="Добавление статьи")
 
 @app.teardown_appcontext
 def close_db(error):
     '''Закрываем соединение с БД, если оно было установлено'''
     if hasattr(g, 'link_db'):
         g.link_db.close()
+
+@app.route('/post/<int:id_post>')
+def showPost(id_post):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.getPost(id_post)
+    if not title:
+        abort(404)
+
+    return render_template('post.html', menu = dbase.getMenu(), title=title, post=post)
 
 if __name__ == '__main__':
     app.run(debug=True)
